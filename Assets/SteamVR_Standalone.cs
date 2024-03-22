e8e573141884a694f780c2ec890679a1b34fec55
Jason Deacutis <jdeacutis@inter-coastal.net>
2024-03-22 12:28:37 -0700

    Improved classes

--- Assets/SteamVR_Standalone.cs
+++ Assets/SteamVR_Standalone.cs
@@ -1,19 +1,25 @@
+using System;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Valve.VR;
 
 public class SteamVR_Standalone : MonoBehaviour {
     public static SteamVR_Standalone Instance;
+    public static event Action<TrackedDevicePose_t[]> OnNewPose;
 
     public TrackedDevicePose_t[] poses = new TrackedDevicePose_t[OpenVR.k_unMaxTrackedDeviceCount];
     public TrackedDevicePose_t[] gamePoses = new TrackedDevicePose_t[0];
 
     void UpdatePoses() {
-        CVRCompositor compositor = OpenVR.Compositor;
-        if (compositor != null) {
-            compositor.GetLastPoses(poses, gamePoses);
-        }
+        OpenVR.Compositor?.GetLastPoses(poses, gamePoses);
+        OnNewPose?.Invoke(poses);
     }
 
-    void OnEnable() => Instance = this;
+    void OnEnable() {
+        Instance = this;
+        RenderPipelineManager.beginFrameRendering += OnBeginFrameRendering;
+    }
+    void OnDisable() => RenderPipelineManager.beginFrameRendering -= OnBeginFrameRendering;
     void Update() => UpdatePoses();
+    void OnBeginFrameRendering(ScriptableRenderContext context, Camera[] cameras) => UpdatePoses();
 }
--- Assets/SteamVR_Tracker.cs
+++ Assets/SteamVR_Tracker.cs
@@ -1,6 +1,5 @@
-using Unity.Mathematics;
+using System.Diagnostics;
 using UnityEngine;
-using Util;
 using Valve.VR;
 
 public class SteamVR_Tracker : MonoBehaviour {
@@ -25,17 +24,20 @@ public class SteamVR_Tracker : MonoBehaviour {
         Device16
     }
 
-    public Device device;
+    public Device device = Device.None;
     public bool setPosition = true;
     public bool setRotation = true;
+    public float positionSmoothTime = 0f;
     public float rotationSmoothTime = 0f;
-    public float3 velocity;
-    public float3 angularVelocity;
+    public Vector3 velocity;
+    public Vector3 angularVelocity;
     public ETrackingResult tracking;
 
-    Quaternion smoothed_rot;
+    Stopwatch sw = new();
+    Vector3 pos;
+    Vector3 pos_vel;
+    Quaternion rot;
     Quaternion rot_vel;
-    bool initial = true;
 
     public static Matrix4x4 OVRToMatrix4x4(HmdMatrix34_t m_pose) {
         Matrix4x4 m = default;
@@ -58,29 +60,63 @@ public class SteamVR_Tracker : MonoBehaviour {
         return m;
     }
 
-    void Update() {
+    public static Quaternion QuatSmoothDamp(Quaternion current, Quaternion target, ref Quaternion vel, float smoothTime, float deltaTime) {
+        //if (deltaTime < Mathf.Epsilon) return current;
+        if (smoothTime < Mathf.Epsilon) return target;
+        // account for double-cover
+        float Dot = Quaternion.Dot(current, target);
+        float Multi = Dot > 0f ? 1f : -1f;
+        target.x *= Multi;
+        target.y *= Multi;
+        target.z *= Multi;
+        target.w *= Multi;
+        // smooth damp (nlerp approx)
+        Vector4 Result = new Vector4(
+                Mathf.SmoothDamp(current.x, target.x, ref vel.x, smoothTime, float.PositiveInfinity, deltaTime),
+                Mathf.SmoothDamp(current.y, target.y, ref vel.y, smoothTime, float.PositiveInfinity, deltaTime),
+                Mathf.SmoothDamp(current.z, target.z, ref vel.z, smoothTime, float.PositiveInfinity, deltaTime),
+                Mathf.SmoothDamp(current.w, target.w, ref vel.w, smoothTime, float.PositiveInfinity, deltaTime)
+            ).normalized;
+
+        // ensure deriv is tangent
+        Vector4 derivError = Vector4.Project(new Vector4(vel.x, vel.y, vel.z, vel.w), Result);
+        vel.x -= derivError.x;
+        vel.y -= derivError.y;
+        vel.z -= derivError.z;
+        vel.w -= derivError.w;
+
+        return new Quaternion(Result.x, Result.y, Result.z, Result.w);
+    }
+
+    public static Quaternion GetMatrixRotation(Matrix4x4 matrix) => Quaternion.LookRotation(new Vector3(matrix.m02, matrix.m12, matrix.m22), new Vector3(matrix.m01, matrix.m11, matrix.m21));
+
+    void UpdateTransform(TrackedDevicePose_t[] poses) {
         if (device == Device.None) return;
 
-        TrackedDevicePose_t[] poses = SteamVR_Standalone.Instance.poses;
-
-        int i_device = (int)device;
-        if (poses.Length <= i_device) return;
-
-        TrackedDevicePose_t pose = poses[i_device];
+        TrackedDevicePose_t pose = poses[(int)device];
         if (!pose.bDeviceIsConnected || !pose.bPoseIsValid) return;
 
         Matrix4x4 m = OVRToMatrix4x4(pose.mDeviceToAbsoluteTracking);
-        Quaternion rot = m.GetRotation();
-
         velocity = new(pose.vVelocity.v0, pose.vVelocity.v1, pose.vVelocity.v2);
         angularVelocity = new(pose.vAngularVelocity.v0, pose.vAngularVelocity.v1, pose.vAngularVelocity.v2);
         tracking = pose.eTrackingResult;
 
-        smoothed_rot = MathUtil.SmoothDamp(initial ? rot : smoothed_rot, rot, ref rot_vel, rotationSmoothTime);
+        float dt = (float)sw.Elapsed.TotalSeconds;
+        sw.Restart();
 
-        if (setPosition) transform.localPosition = m.GetPosition();
-        if (setRotation) transform.localRotation = smoothed_rot;
-
-        initial = false;
+        if (setPosition) {
+            pos = Vector3.SmoothDamp(pos, m.GetPosition(), ref pos_vel, positionSmoothTime, float.PositiveInfinity, dt);
+            transform.localPosition = pos;
+        }
+        if (setRotation) {
+            rot = QuatSmoothDamp(rot, GetMatrixRotation(m), ref rot_vel, rotationSmoothTime, dt);
+            transform.localRotation = rot;
+        }
     }
+    void OnEnable() {
+        transform.GetLocalPositionAndRotation(out pos, out rot);
+        SteamVR_Standalone.OnNewPose += UpdateTransform;
+    }
+
+    void OnDisable() => SteamVR_Standalone.OnNewPose -= UpdateTransform;
 }
